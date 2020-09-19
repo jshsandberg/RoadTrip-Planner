050696b453efa426899fc4bcac919cba219c633d
Eric Folenta <ericfolenta@gmail.com>
2020-09-19 12:22:32 -0700

    added seeds file with 50 states and updated schema for better execution

--- schema.sql
+++ schema.sql
@@ -2,3 +2,10 @@
 DROP DATABASE IF EXISTS statemap;
 -- Creates the "statemap" database --
 CREATE DATABASE statemap;
+USE statemap;
+CREATE TABLE states (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30),
+  abbr VARCHAR(2),
+  PRIMARY KEY (id)
+);
