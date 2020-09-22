479bf9bb7ebd8b0b904c97adb60127d90aaea818
b1acksmyth <smith.spence13@gmail.com>
2020-09-21 20:56:51 -0700

    server now has handlebars included

--- server.js
+++ server.js
@@ -1,18 +1,15 @@
-// *****************************************************************************
-// Server.js - This file is the initial starting point for the Node/Express server.
-//
-// ******************************************************************************
-// *** Dependencies
-// =============================================================
-var express = require("express");
 
-// Sets up the Express App
-// =============================================================
-var app = express();
-var PORT = process.env.PORT || 3000;
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const expbs = require("express-handlebars");
+
+
+app.engine("handlebars", expbs());
+app.set("view engine", "handlebars");
 
 // Requiring our models for syncing
-var db = require("./models");
+const db = require("./models");
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -22,7 +19,6 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Routes
-
 require("./routes/api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
