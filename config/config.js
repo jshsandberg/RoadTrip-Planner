cfcd047c0fa4afdfc89dac83967f880c91ef8d92
Eric Folenta <ericfolenta@gmail.com>
2020-09-28 17:41:35 -0700

    updated config

--- config/config.js
+++ config/config.js
@@ -13,19 +13,25 @@ module.exports = {
     }
   },
   "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PWD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "port": 3306,
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "define": {
+      "timestamps": false
+    }
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": process.env.DB_USER,
+    "password": process.env.DB_PWD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "port": 3306,
-    "dialect": "mysql"
+    "dialect": "mysql",
+    "define": {
+      "timestamps": false
+    }
   }
 }
