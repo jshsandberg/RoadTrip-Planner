1c23f335b35fdff9fe3cf654f8a7e154d27353ca
Eric Folenta <ericfolenta@gmail.com>
2020-09-28 21:14:41 -0700

    reverted index

--- models/index.js
+++ models/index.js
@@ -9,14 +9,11 @@ const config = require(__dirname + '/../config/config.js')[env];
 const db = {};
 
 let sequelize;
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL, config);
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
-
-
-
 
 fs
   .readdirSync(__dirname)
